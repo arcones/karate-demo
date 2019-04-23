c548a5b59062f1b1ab186d9ca4d413fbb4fb395c
Arcones <marta.arconesrodriguez@zooplus.com>
2019-04-23 17:12:09 +0200

    Complete first version of the script

--- karate-linux.sh
+++ karate-linux.sh
@@ -1,1 +1,7 @@
-echo Hello
+#!/bin/bash
+
+git clone https://github.com/arcones/karate-apiDaysMad19.git
+
+##Hacerlo luego sin suponer que java está instalado
+
+cd karate-apiDaysMad19 && java -jar karate.jar * && xdg-open target/cucumber-html-reports/overview-features.html
